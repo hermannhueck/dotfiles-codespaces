90d0181fa4f132df1b32c63547b65021e75f1abe
Hermann Hueck <hermann.hueck@mailbox.org>
2022-06-14 15:34:39 +0200

    modify setup script

--- setup.sh
+++ setup.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 #
 
-echo "Setting up the codespace environment at $(date)" | tee $HOME/setup.log
+echo "Setting up the codespace environment at $(date)" | tee -a $HOME/setup.log
 
 set -x
 
-pwd | tee $HOME/setup.log
-ls -al | tee $HOME/setup.log
+pwd | tee -a $HOME/setup.log
+ls -al | tee -a $HOME/setup.log
 
-echo $HOME | tee $HOME/setup.log
-echo ~ | tee $HOME/setup.log
-sleep 10 | tee $HOME/setup.log
+echo $HOME | tee -a $HOME/setup.log
+echo ~ | tee -a $HOME/setup.log
+sleep 10 | tee -a $HOME/setup.log
 
 # chmod +x bin/*
 # bin/packages-outdated
@@ -19,10 +19,10 @@ sleep 10 | tee $HOME/setup.log
 
 set +x
 
-echo "Terminated codespace environment setup at $(date)" | tee $HOME/setup.log
+echo "Terminated codespace environment setup at $(date)" | tee -a $HOME/setup.log
 
-pwd | tee $HOME/setup.log
-ls -al | tee $HOME/setup.log
+pwd | tee -a $HOME/setup.log
+ls -al | tee -a $HOME/setup.log
 
-echo $HOME | tee $HOME/setup.log
-echo ~ | tee $HOME/setup.log
+echo $HOME | tee -a $HOME/setup.log
+echo ~ | tee -a $HOME/setup.log
