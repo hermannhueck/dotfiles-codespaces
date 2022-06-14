cf9b3018e633e4ca0780d46851ecbcd8a759b053
Hermann Hueck <hermann.hueck@mailbox.org>
2022-06-14 18:49:54 +0200

    improve setup script with logging

--- setup.sh
+++ setup.sh
@@ -1,14 +1,30 @@
 #!/bin/bash
+#
 
-# copy from dotfiles directory to $HOME
+logfile="$HOME/setup_$(date).log"
+
+echo "$(date):  Setting up the codespace environment ..." >> $logfile
+
+echo "$(date):  Copying from dotfiles directory to $HOME ..." >> $logfile
 cp -r .??* bin ~
 cat my-profile >> ~/.profile
 cat my-profile >> ~/.bashrc
 chmod +x ~/bin/*
+echo "$(date):  Copying DONE." >> $logfile
 
 cd
 
 # run scripts
+echo "$(date):  Updating Ubuntu packages ..." >> $logfile
 bin/packages-outdated && bin/packages-upgrade
+echo "$(date):  Updating Ubuntu DONE." >> $logfile
+
+echo "$(date):  Installing additional packages ..." >> $logfile
 sudo apt install --yes tree wget 
+echo "$(date):  Installing DONE." >> $logfile
+
+echo "$(date):  Setting up Scala Tools ..." >> $logfile
 bin/scala-tools-setup 
+echo "$(date):  Scala Tools Setup DONE." >> $logfile
+
+echo "$(date):  Codespace Environment Setup DONE." >> $logfile
