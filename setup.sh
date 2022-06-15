ff5a26e180b8fe4e2d706c15b8cade076e514dac
Hermann Hueck <hermann.hueck@mailbox.org>
2022-06-15 11:51:12 +0200

    beautify setup output

--- setup.sh
+++ setup.sh
@@ -20,32 +20,47 @@ function done_or_failed {
 
 exec >> $logfile 2>&1
 
+echo "============================================================"
 log "===" "Setting up the codespace environment ..."
 
+echo
+echo "------------------------------------------------------------"
 log ">>>" "Copying from dotfiles directory to $HOME ..."
-cp -r .??* bin ~
-cat my-profile >> ~/.profile
-cat my-profile >> ~/.bashrc
+cp -r .??* bin ~ &&
+cat my-profile >> ~/.profile &&
+cat my-profile >> ~/.bashrc &&
 chmod +x ~/bin/*
-log "<<<" "Copying DONE."
+done_or_failed $? "Copying DONE."
+echo "------------------------------------------------------------"
 
 cd
 
 # run scripts
+echo
+echo "------------------------------------------------------------"
 log ">>>" "Setting up Scala Tools ..."
 bin/scala-tools-setup
 done_or_failed $? "Scala Tools Setup"
+echo "------------------------------------------------------------"
 
+echo
+echo "------------------------------------------------------------"
 log ">>>" "Installing additional tools ..."
 sudo apt-get update &&
   sudo apt install --yes tree direnv fzf bat &&
   cd /usr/bin && sudo ln -s batcat bat && cd -
 done_or_failed $? "Installation of tools"
+echo "------------------------------------------------------------"
 
+echo
+echo "------------------------------------------------------------"
 log ">>>" "Updating Ubuntu packages ..."
 sudo apt-get upgrade --yes &&
   sudo apt-get autoclean --yes &&
   sudo apt-get autoremove --yes
 done_or_failed $? "Updating Ubuntu"
+echo "------------------------------------------------------------"
 
+echo
 log "===" "Codespace Environment Setup TERMINATED."
+echo "============================================================"
